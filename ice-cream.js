09fa604487352752b19d37facff66752eb2e2233
Kenny Lee <kenny.lee927@gmail.com>
2023-06-13 12:22:51 -0400

    complete ice-cream prep activity

---
+++ ice-cream.js
@@ -0,0 +1,22 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry"
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Get the First Flavour
+console.log(iceCreamFlavours[0]);
+
+// Get the Last Flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// Total Number of Flavours
+console.log(iceCreamFlavours.length);
