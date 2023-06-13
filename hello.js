22d5120cfe74912c70b9b4aa71723d79ca3e1121
Kenny Lee <kenny.lee927@gmail.com>
2023-06-13 10:09:41 -0400

    add to sayHello functions, practice isEven function

--- hello.js
+++ hello.js
@@ -1,8 +1,19 @@
-const sayHello  = function (name) {
+const sayHello = function (name) {
   console.log("Hello, " + name);
-}
+};
 
 sayHello();
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+};
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+const greeting = returnSayHello('John');
+console.log(greeting);
