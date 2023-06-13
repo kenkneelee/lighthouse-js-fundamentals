f3f1b6db10b85167fddca55b63e1b0f6238c9fad
Kenny Lee <kenny.lee927@gmail.com>
2023-06-13 14:06:47 -0400

    complete concat and merge array activities

---
+++ mergeArrays.js
@@ -0,0 +1,8 @@
+function merge (a, b) {
+  // Use array method concat() and sort()
+  return a.concat(b).sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
