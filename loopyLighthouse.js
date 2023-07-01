7174e3efc9f92b8fdfb4c18d2d1e9775497da4b8
Kenny Lee <kenny.lee927@gmail.com>
2023-07-01 00:54:52 -0400

    update for loopy lighthouse 2 - change constants to given input variables

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,20 +1,22 @@
-// Use for loop to set 100 as start point, 200 as end point, and increment by 1
-for (let i = 100; i <= 200; i++) {
-  // First, check to see if both conditions are true (divisible by 3 and 4)
-  if (i % 3 == 0 && i % 4 == 0) {
-    console.log("LoopyLighthouse");
+const loopyLighthouse = function (range, multiples, words) {
+  // Use for loop to set 100 as start point, 200 as end point, and increment by 1
+  for (let i = range[0]; i <= range[1]; i++) {
+    // First, check to see if both conditions are true (divisible by 3 and 4)
+    if (i % multiples[0] == 0 && i % multiples[1] == 0) {
+      console.log(words[0] + words[1]);
+    }
+    // If not, check if each individual condition is true
+    // Divisible by 3
+    else if (i % multiples[0] == 0) {
+      console.log(words[0]);
+    }
+    // Divisible by 4
+    else if (i % multiples[1] == 0) {
+      console.log(words[1]);
+    }
+    // If neither condition is true then print the number
+    else {
+      console.log(i);
+    }
   }
-  // If not, check if each individual condition is true
-  // Divisible by 3
-  else if (i % 3 == 0) {
-    console.log("Loopy");
-  }
-  // Divisible by 4
-  else if (i % 4 == 0) {
-    console.log("Lighthouse");
-  }
-  // If neither condition is true then print the number
-  else {
-    console.log(i);
-  }
-}
+};
